604b6c437f7d1a12d3328ea38c72058339f41b02
“[emanuel <eflobau@gmail.com>
2019-02-23 20:57:23 -0600

    changed all notebooks, added data

--- notebooks/TCD19_utils.py
+++ notebooks/TCD19_utils.py
@@ -4,14 +4,15 @@ import matplotlib as mpl
 from matplotlib import rcParams
 rcParams['axes.titlepad'] = 20 
 
+
 def set_plotting_style():
       
-      tw = 1.5
-      rc = {'lines.linewidth': 2,
-            'axes.labelsize': 22,
-            'axes.titlesize': 24,
-            'xtick.major' : 22,
-            'ytick.major' : 22,
+    tw = 1.5
+    rc = {'lines.linewidth': 2,
+            'axes.labelsize': 18,
+            'axes.titlesize': 21,
+            'xtick.major' : 16,
+            'ytick.major' : 16,
             'xtick.major.width': tw,
             'xtick.minor.width': tw,
             'ytick.major.width': tw,
@@ -26,42 +27,47 @@ def set_plotting_style():
             'mathtext.fontset': 'stixsans',
             'mathtext.sf': 'fantasy',
             'legend.frameon': True,
-            'legend.fontsize': 22, 
+            'legend.fontsize': 12, 
            "xtick.direction": "in","ytick.direction": "in"}
+    
+    plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
+    plt.rc('mathtext', fontset='stixsans', sf='sans')
+    sns.set_style('ticks', rc=rc)
+    #sns.set_palette("colorblind", color_codes=True)
+    sns.set_context('notebook', rc=rc)
 
 
 def set_plotting_style_2():
       
-      tw = 1.5
-      rc = {'lines.linewidth': 2,
-            'axes.labelsize': 22,
-            'axes.titlesize': 24,
-            'xtick.major' : 22,
-            'ytick.major' : 22,
-            'xtick.major.width': tw,
-            'xtick.minor.width': tw,
-            'ytick.major.width': tw,
-            'ytick.minor.width': tw,
-            'xtick.labelsize': 'large',
-            'ytick.labelsize': 'large',
-            'font.family': 'sans',
-            'weight':'bold',
-            'grid.linestyle': ':',
-            'grid.linewidth': 1.5,
-            'grid.color': '#ffffff',
-            'mathtext.fontset': 'stixsans',
-            'mathtext.sf': 'fantasy',
-            'legend.frameon': True,
-            'legend.fontsize': 22, 
-           "xtick.direction": "in","ytick.direction": "in"}
+    tw = 1.5
+
+    rc = {'lines.linewidth': 2,
+        'axes.labelsize': 18,
+        'axes.titlesize': 21,
+        'xtick.major' : 16,
+        'ytick.major' : 16,
+        'xtick.major.width': tw,
+        'xtick.minor.width': tw,
+        'ytick.major.width': tw,
+        'ytick.minor.width': tw,
+        'xtick.labelsize': 'large',
+        'ytick.labelsize': 'large',
+        'font.family': 'sans',
+        'weight':'bold',
+        'grid.linestyle': ':',
+        'grid.linewidth': 1.5,
+        'grid.color': '#ffffff',
+        'mathtext.fontset': 'stixsans',
+        'mathtext.sf': 'fantasy',
+        'legend.frameon': True,
+        'legend.fontsize': 12, 
+       "xtick.direction": "in","ytick.direction": "in"}
 
 
 
-      plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
-      plt.rc('mathtext', fontset='stixsans', sf='sans')
-      sns.set_style('ticks', rc=rc)
+    plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
+    plt.rc('mathtext', fontset='stixsans', sf='sans')
+    sns.set_style('ticks', rc=rc)
 
-      sns.set_palette("colorblind", color_codes=True)
-      sns.set_context('notebook', rc=rc)
-
-
+    #sns.set_palette("colorblind", color_codes=True)
+    sns.set_context('notebook', rc=rc)
